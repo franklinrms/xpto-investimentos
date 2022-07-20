de6c94265b9db6d41deddbd6de7cac59dad216e9
Franklin Ramos <franklinramos@outlook.com>
2022-07-20 03:03:26 -0300

    feat: implementa botão sair na pagina de perfil

--- mobile/src/pages/Profile/index.jsx
+++ mobile/src/pages/Profile/index.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { User } from 'phosphor-react-native';
+import React, { useContext } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 import Menu from '../../components/Menu';
+import UserContext from '../../context/UserContext';
+import theme from '../../theme';
 
 import styles from './styles';
 
 export default function Profile() {
+  const { user, setUser } = useContext(UserContext);
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
+      <View style={styles.icon}>
+        <User size={36} color={theme.colors.text_secondary} weight="regular" />
+      </View>
+      <Text style={styles.title}>{user.name}</Text>
+      <Text style={styles.title}>{user.email}</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          setUser({});
+          navigation.navigate('Login');
+        }}
+      >
+        <Text style={styles.buttonTitle}>Sair</Text>
+      </TouchableOpacity>
       <Menu />
     </View>
   );
--- mobile/src/pages/Profile/styles.jsx
+++ mobile/src/pages/Profile/styles.jsx
@@ -1,8 +1,40 @@
 import { StyleSheet } from 'react-native';
+import theme from '../../theme';
 
 const styles = StyleSheet.create({
   container: {
     height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: theme.colors.surface_secondary,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 14,
+    color: theme.colors.text_primary,
+    fontFamily: theme.fonts.medium,
+  },
+  button: {
+    height: 60,
+    width: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 16,
+    marginTop: 100,
+    backgroundColor: theme.colors.sell,
+  },
+  buttonTitle: {
+    fontSize: 18,
+    color: theme.colors.text_primary,
+    fontWeight: 'bold',
+    fontFamily: theme.fonts.medium,
   },
 });
 export default styles;
