a0a85c3e6c257befc0993d6cc1a510036f1102ab
Franklin Ramos <franklinramos@outlook.com>
2022-07-21 01:10:26 -0300

    chore: cria arquivo com dados das ações

---
+++ mobile/src/data/STOCKS_DATA.jsx
@@ -0,0 +1,78 @@
+const STOCKS_DATA = [
+  {
+    stockId: 'ABEV3',
+    name: 'Ambev SA',
+    price: 14.59,
+  },
+  {
+    stockId: 'PETR4',
+    name: 'Petrobras',
+    price: 27.96,
+  },
+  {
+    stockId: 'MGLU3',
+    name: 'Magazine Luiza',
+    price: 3.07,
+  },
+  {
+    stockId: 'VIIA3',
+    name: 'Via',
+    price: 2.61,
+  },
+  {
+    stockId: 'CASH3',
+    name: 'Meliuz SA',
+    price: 1.11,
+  },
+  {
+    stockId: 'B3SA3',
+    name: 'B3 SA',
+    price: 10.54,
+  },
+  {
+    stockId: 'COGN3',
+    name: 'Cogna Educação',
+    price: 2.32,
+  },
+  {
+    stockId: 'VALE3',
+    name: 'Vale S.A.',
+    price: 67.39,
+  },
+  {
+    stockId: 'VBBR3',
+    name: 'Vibra Energia',
+    price: 17.02,
+  },
+  {
+    stockId: 'LWSA3',
+    name: 'Locaweb',
+    price: 6.69,
+  },
+  {
+    stockId: 'PETZ3',
+    name: 'Petz',
+    price: 10.69,
+  },
+  {
+    stockId: 'AMER3',
+    name: 'B2W Digital',
+    price: 16.92,
+  },
+  {
+    stockId: 'EMBR3',
+    name: 'Embraer SA',
+    price: 12.15,
+  },
+  {
+    stockId: 'AZUL4',
+    name: 'Azul SA',
+    price: 12.41,
+  },
+  {
+    stockId: 'ALPA4',
+    name: 'Alpargatas S.A.',
+    price: 20.85,
+  },
+];
+export default STOCKS_DATA;
