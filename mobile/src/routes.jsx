03d85483fe8d46bee015a7da8ffbdcbcbcb52ef2
Franklin Ramos <franklinramos@outlook.com>
2022-07-23 15:13:43 -0300

    chore: apaga arquivos não utilizados

--- mobile/src/routes.jsx
+++ mobile/src/routes.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Login from './pages/Login';
-import Home from './pages/Home';
 import Wallet from './pages/Wallet';
 import AllStocks from './pages/AllStocks';
 import StockDetails from './pages/StockDetails';
@@ -20,7 +19,6 @@ function Routes() {
       }}
     >
       <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Wallet" component={Wallet} />
       <Stack.Screen name="AllStocks" component={AllStocks} />
       <Stack.Screen name="StockDetails" component={StockDetails} />
